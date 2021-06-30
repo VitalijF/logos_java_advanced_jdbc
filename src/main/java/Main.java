ca08a30be5c43f70178996312a9aa5b027556fee
sofi2002sofi <54448491+sofi2002sofi@users.noreply.github.com>
2021-06-30 14:52:42 +0300

    add logs for all the methods in BlogService and UserService

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,16 +15,38 @@ import java.time.LocalDate;
 
 public class Main {
 
-    private static BlogService blogService = new BaseBlogService(new MySqlBlogDao());
-    private static UserService userService = new BaseUserService(new MySqlUserDao());
+    private static final BlogService blogService = new BaseBlogService(new MySqlBlogDao());
+    private static final UserService userService = new BaseUserService(new MySqlUserDao());
 
     public static void main(String[] args) throws SQLException, NoSuchBlogException, DuplicateBlogException {
 
+//        User user = new User(
+//                10,
+//                "Sofiia",
+//                "Didula",
+//                LocalDate.of(1972, 2, 3)
+//        );
+        userService.getUserById(5);
+//        System.out.println("Creating user successfully");
         BlogInput blog = new BlogInput(
-                3,
-                "Windows11",
-                2
+                7,
+                "Ubuntu 20.04",
+                10
         );
+
+        System.out.println("Creating Blog");
+        blogService.createBlog(blog);
+        System.out.println("Creating blog successfully");
+
+        blogService.getAllBlogs();
+        System.out.println("Return all Blogs");
+
+        Blog blogById = blogService.getBlogById(blog.getId());
+        System.out.println(blogById);
+
+        System.out.println("Creating Blog");
+        blogService.createBlog(blog);
+        System.out.println("Creating blog successfully");
 //        User user = new User(
 //                2,
 //                "Ivan",
@@ -32,12 +54,12 @@ public class Main {
 //                LocalDate.of(1972, 2, 3)
 //        );
 
-        System.out.println("Creating Blog");
-        blogService.createBlog(blog);
-        System.out.println("Creating blog successfully");
-
-        Blog blogById = blogService.getBlogById(blog.getId());
-        System.out.println(blogById);
+//        System.out.println("Creating Blog");
+//        blogService.createBlog(blog);
+//        System.out.println("Creating blog successfully");
+//
+//        Blog blogById = blogService.getBlogById(blog.getId());
+//        System.out.println(blogById);
 
 //        System.out.println("Creating user");
 //        userService.createUser(user);
--- src/main/java/service/imp/BaseBlogService.java
+++ src/main/java/service/imp/BaseBlogService.java
@@ -7,6 +7,7 @@ import jdbc.MySqlConnector;
 import lombok.SneakyThrows;
 import model.Blog;
 import model.BlogInput;
+import org.apache.log4j.Logger;
 import service.BlogService;
 import service.UserService;
 
@@ -15,6 +16,8 @@ import java.sql.SQLException;
 import java.util.List;
 
 public class BaseBlogService implements BlogService {
+
+    private static final Logger LOGGER = Logger.getLogger(BaseUserService.class);
 
     private BlogDao blogDao;
 
@@ -25,7 +28,8 @@ public class BaseBlogService implements BlogService {
     @Override
     @SneakyThrows
     public List<Blog> getAllBlogs() {
-       return blogDao.getAllBlogs();
+        LOGGER.info("All blogs have been returned");
+        return blogDao.getAllBlogs();
     }
 
     @Override
@@ -33,8 +37,12 @@ public class BaseBlogService implements BlogService {
     public Blog getBlogById(int id) {
         Blog blogById = blogDao.getBlogById(id);
         if (blogById != null){
+            LOGGER.info(String.format("Blog with id = {%d}, name = {%s} and User data - id ={%d}, " +
+                    "first_name = {%s}, last_name = {%s} has been returned", blogById.getId(), blogById.getName(),
+                    blogById.getUser().getId(), blogById.getUser().getFirstName(), blogById.getUser().getLastName() ));
             return blogById;
         }
+        LOGGER.info(String.format("Blog with id = {%d} does not exists", blogById.getId()));
         return null;
     }
 
@@ -43,8 +51,12 @@ public class BaseBlogService implements BlogService {
     public void createBlog(BlogInput blog) {
         Blog blogById = blogDao.getBlogById(blog.getId());
         if(blogById != null){
+            LOGGER.error(String.format("Can NOT create blog with id = {%d}, there is already exists blog with such id " +
+                    "in DB", blogById.getId()));
             throw new RuntimeException("Already existed blog with such id in DB");
         }
+        LOGGER.info(String.format("Blog with id = {%d}, name = {%s} and user_id ={%d} has been created",
+                blog.getId(), blog.getName(), blog.getUserId()));
         blogDao.createBlog(blog);
     }
 
